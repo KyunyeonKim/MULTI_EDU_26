79b5138e8292a4b5a24a9b6807222e55d7ce9cc7
KyunyeonKim <kgn4746@naver.com>
2024-08-31 21:17:31 +0900

    8/31

---
+++ springFrameWork/work/spring14mission_product/src/main/java/test/com/product/controller/ProductController.java
@@ -0,0 +1,131 @@
+package test.com.product.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import test.com.product.model.ProductVO;
+import test.com.product.service.ProductService;
+
+
+@Controller
+public class ProductController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+	
+	@Autowired
+	ProductService service;
+	
+	@RequestMapping(value = "/p_insert.do", method = RequestMethod.GET)
+	public String p_insert() {
+		logger.info("p_insert");
+		
+		return "product/insert";
+	}
+	
+	
+	@RequestMapping(value = "/p_update.do", method = RequestMethod.GET)
+	public String p_update(ProductVO vo,Model model) {
+		logger.info("p_update");
+		logger.info("{}",vo);
+		
+		ProductVO vo2 = service.selectOne(vo);
+		logger.info("vo2:{}",vo2);
+		
+		model.addAttribute("vo2",vo2);
+		
+		return "product/update";
+	}
+	
+	@RequestMapping(value = "/p_delete.do", method = RequestMethod.GET)
+	public String p_delete() {
+		logger.info("p_delete");
+		
+		return "product/delete";
+	}
+	
+	@RequestMapping(value = "/p_selectAll.do", method = RequestMethod.GET)
+	public String p_selectAll(Model model) {
+		logger.info("p_selectAll");
+		
+		List<ProductVO> list = service.selectAll();
+		logger.info("{}",list);
+		
+		model.addAttribute("list",list);
+		
+		return "product/selectAll";
+	}
+	
+	@RequestMapping(value = "/p_searchList.do", method = RequestMethod.GET)
+	public String p_searchList(Model model,@RequestParam(defaultValue = "pname")String searchKey,
+			@RequestParam(defaultValue = "ph")String searchWord) {
+		logger.info("p_searchList");
+		logger.info("searchKey:{}",searchKey);
+		logger.info("searchWord:{}",searchWord);
+		
+		List<ProductVO> list = service.searchList(searchKey,searchWord);
+		logger.info("{}",list);
+		
+		model.addAttribute("list",list);
+		
+		return "product/selectAll";
+	}
+	
+	@RequestMapping(value = "/p_selectOne.do", method = RequestMethod.GET)
+	public String p_selectOne(ProductVO vo,Model model ) {
+		logger.info("p_selectOne");
+		logger.info("{}",vo);
+		
+		ProductVO vo2 = service.selectOne(vo);
+		logger.info("vo2:{}",vo2);
+		
+		model.addAttribute("vo2",vo2);
+		
+		return "product/selectOne";
+	}
+	
+	@RequestMapping(value = "/p_insertOK.do", method = RequestMethod.POST)
+	public String p_insertOK(ProductVO vo) {
+		logger.info("p_insertOK");
+		logger.info("{}",vo);
+		
+		int result = service.insert(vo);
+		logger.info("result:{}",result);
+		
+		return "redirect:p_selectAll.do";
+	}
+	
+	@RequestMapping(value = "/p_updateOK.do", method = RequestMethod.POST)
+	public String p_updateOK(ProductVO vo) {
+		logger.info("p_updateOK");
+		logger.info("{}",vo);
+		
+		int result = service.update(vo);
+		logger.info("result:{}",result);
+		
+		return "redirect:p_selectAll.do";
+	}
+	
+	@RequestMapping(value = "/p_deleteOK.do", method = RequestMethod.POST)
+	public String p_deleteOK(ProductVO vo) {
+		logger.info("p_deleteOK");
+		logger.info("{}",vo);
+		
+		int result = service.delete(vo);
+		logger.info("result:{}",result);
+		
+		return "redirect:p_selectAll.do";
+	}
+	
+	
+	
+	
+	
+}//end class
---
+++ springFrameWork/work/spring14mission_product/src/main/webapp/WEB-INF/views/product/insert.jsp
@@ -0,0 +1,96 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+      <style>
+    input[type=text],
+    textarea,
+    select {
+      
+      padding: 8px 8px;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ff8f8f;
+      border-radius: 14px;
+      box-sizing: border-box;
+    }
+
+    input[type=submit] ,a{
+      width: 15%;
+      background-color: #4CAF50;
+      color: white;
+      padding: 8px 8px;
+      margin: 8px 0px;
+      border: none;
+      border-radius: 14px;
+      cursor: pointer;
+    }
+
+    input[type=submit]:hover {
+      background-color: #b7e5b9;
+    }
+
+    div {
+      border-radius: 15px;
+      background-color: #f0f0f0;
+      margin: 20px;
+    }
+
+    #insertTable {
+      font-family: Arial, Helvetica, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    }
+
+    #insertTable td,th {
+      border: 1px solid #ddd;
+      padding: 8px;
+    }
+
+    #insertTable tr:nth-child(even) {
+      background-color: #ebebeb;
+    }
+
+    #insertTable tr:hover {
+      background-color: #ffc6c6;
+    }
+  </style>
+</head>
+
+<body>
+<h1>제품등록 페이지</h1>
+<jsp:include page="../top_menu.jsp"/>
+<div>
+  
+  <form action="p_insertOK.do" method="post">
+    <table id="insertTable">
+      <tr>
+        <td><label for="pname">pname</label></td>
+        <td><input type="text" id="pname" name="pname" value="BMW" placeholder="pname를 입력하세요"></td>
+      </tr>
+      <tr>
+        <td><label for="model">model</label></td>
+        <td><input type="text" id="model" name="model" value="B119" placeholder="model를 입력하세요"></td>
+      </tr>
+      <tr>
+        <td><label for="price">price</label></td>
+        <td><input type="number" id="price" name="price" value="3000" min="0" step="1000" placeholder="price를 입력하세요"></td>
+      </tr>
+      <tr>
+        <td><label for="count">count</label></td>
+        <td><input type="number" id="count" name="count" value="10" placeholder="count를 입력하세요"></td>
+      </tr>
+      <tr>
+        <td colspan="2"><input type="submit" value="등록완료"></td>
+      </tr>
+    </table>
+  </form>
+</div>
+</body>
+
+</html>
